021348c6bb6f8b5a094fdeffcf92d3049cf3bba5
heyoulin <heyoulin@yofc.com>
2022-01-24 01:32:04 +0800

    CamelProduce bug

--- kura/org.eclipse.kura.wire.camel/src/main/java/org/eclipse/kura/wire/camel/CamelProduce.java
+++ kura/org.eclipse.kura.wire.camel/src/main/java/org/eclipse/kura/wire/camel/CamelProduce.java
@@ -15,18 +15,41 @@ package org.eclipse.kura.wire.camel;
 import static org.apache.camel.impl.engine.DefaultFluentProducerTemplate.on;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.FluentProducerTemplate;
 import org.eclipse.kura.wire.WireEnvelope;
 
 public class CamelProduce extends AbstractReceiverWireComponent {
 
+    private FluentProducerTemplate template = null;
+
     @Override
     protected void processReceive(final CamelContext context, final String endpointUri, final WireEnvelope envelope)
             throws Exception {
-
-        on(context) //
+        if (template == null) {
+            template = on(context);
+        } else {
+            if (template.getCamelContext() != context) {
+                template.close();
+                template = on(context);
+            }
+        }
+        template //
                 .withBody(envelope) //
                 .to(endpointUri) //
                 .asyncSend();
+    }
+
+    @Override
+    protected void deactivate() {
+        super.deactivate();
+        if (template != null) {
+            try {
+                template.close();
+            } catch (Exception e) {
+            } finally {
+                template = null;
+            }
+        }
     }
 
 }
