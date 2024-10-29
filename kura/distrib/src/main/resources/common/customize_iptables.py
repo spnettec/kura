1a95d9f6fc31f06259461b951129fc70a1bba2e0
heyoulin <heyoulin@hotmail.com>
2024-10-29 18:22:38 +0800

    fix wifi

--- kura/distrib/src/main/resources/common/customize_iptables.py
+++ kura/distrib/src/main/resources/common/customize_iptables.py
@@ -42,19 +42,22 @@ def main():
         iptables_content = iptables.read()
     
     iptables_content_updated = ""
+    lines = iptables_content.split("\n")
     if wlan_number == 0:
-        for line in iptables_content.split("\n"):
-            if 'WIFI_INTERFACE_0' not in line:
-                if eth_number > 1:
-                    iptables_content_updated += (line.replace('ETH_INTERFACE_0', eth_names[1]) + "\n")
-                else:
-                    iptables_content_updated += (line.replace('ETH_INTERFACE_0', eth_names[0]) + "\n")
+	    if eth_number == 1:
+	        lines = [line for line in lines if not any(exc in line for exc in ["WIFI_INTERFACE_0","ETH_INTERFACE_1"])]
+	    else:
+	        lines = [line for line in lines if not any(exc in line for exc in ["WIFI_INTERFACE_0"])]
     else:
-        iptables_content_updated = iptables_content
-        if eth_number > 1:
-            iptables_content_updated = iptables_content_updated.replace('ETH_INTERFACE_0', eth_names[1])
-        else:
-            iptables_content_updated = iptables_content_updated.replace('ETH_INTERFACE_0', eth_names[0])
+        if eth_number == 1:
+            lines = [line for line in lines if not any(exc in line for exc in ["ETH_INTERFACE_1"])]
+    iptables_content_updated = '\n'.join(lines)
+    if eth_number > 1:
+        iptables_content_updated = iptables_content_updated.replace('ETH_INTERFACE_0', eth_names[0])
+        iptables_content_updated = iptables_content_updated.replace('ETH_INTERFACE_1', eth_names[1])
+    else:
+        iptables_content_updated = iptables_content_updated.replace('ETH_INTERFACE_0', eth_names[0])
+    if wlan_number > 0:
         iptables_content_updated = iptables_content_updated.replace('WIFI_INTERFACE_0', wlan_names[0])
     
     with open(IPTABLES_FILENAME, 'w', encoding='utf-8') as iptables:
