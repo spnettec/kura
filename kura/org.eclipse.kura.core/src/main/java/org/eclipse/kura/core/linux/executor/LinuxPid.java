92210ed42f8300acd460556eb30024aeef46f4ba
heyoulin <heyoulin@gmail.com>
2024-11-03 17:03:14 +0800

    format

--- kura/org.eclipse.kura.core/src/main/java/org/eclipse/kura/core/linux/executor/LinuxPid.java
+++ kura/org.eclipse.kura.core/src/main/java/org/eclipse/kura/core/linux/executor/LinuxPid.java
@@ -41,12 +41,15 @@ public class LinuxPid implements Pid {
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
+        if (this == obj) {
             return true;
-        if (obj == null)
+        }
+        if (obj == null) {
             return false;
-        if (getClass() != obj.getClass())
+        }
+        if (getClass() != obj.getClass()) {
             return false;
+        }
         LinuxPid other = (LinuxPid) obj;
         return pid == other.pid;
     }
